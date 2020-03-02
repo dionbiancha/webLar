c82a7eb19d31be2db0ddfe38c1a0fd71ba6c3c86
dionbiancha <dion.biancha@gmail.com>
2020-03-02 19:11:32 -0300

    initial Redux

--- js/validation.js
+++
@@ -1,9 +0,0 @@
-let form = document.getElementById('formulario');
-var nome = document.getElementById('nome');
-
-form.addEventListener('submit', function(e) {
-
-    alert("Obrigado pelo contato "+nome.value);
-
-    e.preventDefault();
-});
